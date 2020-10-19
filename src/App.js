013068b537fb151cf1d995eefe644937ce5bacb3
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-10-19 15:22:57 +0100

    add accordion component with item props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion';
 
-function App() {
+const items = [
+  {
+    title: "What is the best Star Wars film?",
+    content: "Episode V, The Empire Strikes Back"
+  },
+  {
+    title: "What is the best food to get when eating out?",
+    content: "Chips!"
+  },
+  {
+    title: "Where is the best place to meet friends during lockdown?",
+    content: "Under the heavens on a starry, starry night"
+  }
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion
+        items={items}
+      />
     </div>
   );
-}
-
-export default App;
+};
