6df19a724dd878adf7a4e12e138db7079854ba26
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-10-27 16:07:39 +0000

    use method 3 to add async to useEffect method

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    axios.get('some url')
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
