d11e878064ea70a5532925cb50991b51fe073ad0
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-11-03 12:45:46 +0000

    Options displayed to user in static dropdown menu, with selected item as label

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -17,10 +18,31 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: 'The colour red',
+    value: 'red'
+  },
+  {
+    label: 'The colour green',
+    value: 'green'
+  },
+  {
+    label: 'A shade of blue',
+    value: 'blue'
+  }
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search/>
+      <Dropdown
+        selected={selected}
+        options={options}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
