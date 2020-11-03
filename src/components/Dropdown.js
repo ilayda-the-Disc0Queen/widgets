9c25d8511f32ec499e15c035859eb531506cd92f
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-11-03 16:56:42 +0000

    add toggle button to App to entirely hide dropdown widget

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,14 +5,20 @@ const Dropdown = ({options, selected, onSelectedChange }) => {
   const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener('click', (event) => {
+    const onBodyClick = (event) => {
       // console.log(event.target);
       if (ref.current.contains(event.target)) {
         return;
       }
 
       setOpen(false);
-    });
+    }
+    document.body.addEventListener('click', onBodyClick);
+
+    return () => {
+      document.body.removeEventListener('click', onBodyClick);
+    };
+
   }, []);
 
   const renderedOptions = options.map((option) => {
