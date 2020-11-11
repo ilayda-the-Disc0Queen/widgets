8bc84241698f044e4805733c0a5a2379fa8aef1f
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-11-11 15:28:24 +0000

    add basic routing logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import Accordion from './components/Accordion';
+import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
@@ -34,13 +34,41 @@ const options = [
   }
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+}
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+}
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+}
+
+
 export default () => {
   // const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <Translate/>
-    </div>
+      {showAccordion()}
+      {showList()}
+      {showTranslate()}
+      {showDropdown()}
+    </div>;
   );
 };
